f661811bf1935768ec7bb2163445c99937fc1ff5
lowlowCodee <laurenceg079@gmail.com>
2024-12-20 17:24:27 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,106 @@
+let accounts = JSON.parse(localStorage.getItem('accounts')) || [];
+
+let save = () => {
+    localStorage.setItem('accounts', JSON.stringify(accounts));
+};
+let editIndex = -1;
+
+let addTable = () => {
+    let accTable = document.getElementById('account-list');
+    accTable.innerHTML = '';
+
+    if (accounts.length === 0) {
+        let noRec = document.createElement('tr');
+        noRec.innerHTML = `
+                    <td colspan="4" class="text-center bg-danger-subtle fw-bold fs-5">No Records</td>
+                `;
+
+        accTable.append(noRec);
+        return;
+    }
+
+    for (let i = 0; i < accounts.length; i++) {
+        let account = accounts[i];
+        let Row = document.createElement('tr');
+
+        Row.innerHTML = `
+                <td>${i + 1}</td>
+                <td>${account.email}</td>
+                <td>${account.password}</td>
+                <td>
+                    <button class="btn btn-md btn-warning mt-2 edit"><ion-icon name="create-outline"></ion-icon></button>
+                    <button class="btn btn-md btn-danger mt-2 delete"><ion-icon name="trash-outline"></ion-icon></button> 
+                </td>
+            `;
+        Row.querySelector('.edit').addEventListener('click', function () {
+            document.getElementById('email').value = account.email;
+            document.getElementById('password').value = account.password;
+            editIndex = [i];
+        });
+
+        Row.querySelector('.delete').addEventListener('click', function () {
+            accounts.splice(i, 1);
+            showAlert('Account Deleted', 'danger');
+            save();
+            addTable();
+        });
+
+        accTable.append(Row);
+    }
+};
+addTable();
+
+document
+    .getElementById('account-form')
+    .addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        let email = document.getElementById('email').value.trim();
+        let password = document.getElementById('password').value.trim();
+
+        if (email && password) {
+            showAlert('Account added successfully', 'success');
+        } else {
+            showAlert('Please Fill all fields!', 'danger');
+            return;
+        }
+
+        let account = {
+            email: email,
+            password: password,
+        };
+
+        if (editIndex === -1) {
+            accounts.push(account);
+        } else {
+            accounts[editIndex] = account;
+            showAlert('Edited Successfully', 'success');
+            editIndex = -1;
+        }
+
+        save();
+        addTable();
+
+        document.getElementById('email').value = '';
+        document.getElementById('password').value = '';
+    });
+
+function showAlert(message, type) {
+    let alert = document.getElementById('alert');
+    alert.textContent = message;
+    alert.className = `alert alert-${type}`;
+    alert.classList.remove('d-none');
+
+    setTimeout(function () {
+        alert.classList.add('d-none');
+    }, 1700);
+}
+
+document.getElementById('deleteAll').addEventListener('click', function () {
+    let cnfrm = confirm('are you sure you want to delete all records');
+    if (cnfrm) {
+        accounts = [];
+        save();
+        addTable();
+    }
+});
